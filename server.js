66b093e699adc234b37ead0faa3f2009d2490a12
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2025-01-17 19:23:36 +0000

    update

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(
   expressSession({
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      // httpOnly: true,
+      // secure: process.env.NODE_ENV === 'production',
+      // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     },
     secret: process.env.SESSION_SECRET,
     resave: false,
