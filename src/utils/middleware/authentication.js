3d7951361cfd5d357534f7e1c54125144159e7b0
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2025-05-22 21:04:50 +0000

    updates

--- src/utils/middleware/authentication.js
+++ src/utils/middleware/authentication.js
@@ -3,7 +3,6 @@ export default (req, res, next) => {
     // If the user is authenticated, proceed to the next middleware or route handler
     return next();
   }
-  // If the user is not authenticated, redirect them to the login page or send an error
   res
     .status(401)
     .json({ message: 'Unauthorized: Please log in to access this resource.' });
